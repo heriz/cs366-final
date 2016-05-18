dfd6ca50f1110003b4f53792993bbe81c5972d8c
Henry Rizzi <osawari112@gmail.com>
2016-05-18 19:48:32 -0400

    changed naming

--- run.py
+++ run.py
@@ -32,47 +32,47 @@ def exit_program(button):
     raise urwid.ExitMainLoop()
 
 menu_top = menu(u'Style of Message', [
-    sub_menu(u'Formal', [
-        menu_button(u'Formal Markov Chain', item_chosen),
-        menu_button(u'Formal Naive', item_chosen),
-        menu_button(u'Formal Word Replacement', item_chosen),
+    sub_menu(u'Cappy', [
+        menu_button(u'Cappy Markov Chain', item_chosen),
+        menu_button(u'Cappy Naive', item_chosen),
+        menu_button(u'Cappy Word Replacement', item_chosen),
     ]),
-    sub_menu(u'Medium Formal', [
-        menu_button(u'Medium Formal Markov Chain', item_chosen),
-        menu_button(u'Medium Formal Naive', item_chosen),
-        menu_button(u'Medium Formal Word Replacement', item_chosen),
+    sub_menu(u'Wordsmiths', [
+        menu_button(u'Wordsmiths Markov Chain', item_chosen),
+        menu_button(u'Wordsmiths Naive', item_chosen),
+        menu_button(u'Wordsmiths Word Replacement', item_chosen),
     ]),
 
-    sub_menu(u'Informal', [
-        menu_button(u'Informal Markov Chain', item_chosen),
-        menu_button(u'Informal Naive', item_chosen),
-        menu_button(u'Informal Word Replacement', item_chosen),
+    sub_menu(u'Outings Club', [
+        menu_button(u'Outings Markov Chain', item_chosen),
+        menu_button(u'Outings Naive', item_chosen),
+        menu_button(u'Outings Word Replacement', item_chosen),
     ]),
 ])
 
 def handle_choices(choice):
-  if (choice == 'Formal Markov Chain'):
+  if (choice == 'Cappy Markov Chain'):
     markov.generate_email("data/cleaned/greetings-cappy.txt","data/cleaned/cappy.txt",
         "data/cleaned/closings-cappy.txt","gen.txt",par_min=4,par_max=8,sen_min=4,sen_max=8)
-  elif (choice == 'Formal Naive'):
+  elif (choice == 'Cappy Naive'):
     naive_tags.replace_and_output("data/naive/cappy.outline", 
         "gen.txt","data/naive/cappy.yaml")
-  elif (choice == 'Formal Word Replacement'):
+  elif (choice == 'Cappy Word Replacement'):
     markov.generate_email("data/cleaned/greetings-cappy.txt","data/cleaned/cappy.txt",
         "data/cleaned/closings-cappy.txt","gen.txt", replace=True,par_min=4,par_max=8,sen_min=4,sen_max=8)
-  elif (choice == 'Medium Formal Markov Chain'):
+  elif (choice == 'Wordsmiths Markov Chain'):
     markov.generate_email("data/cleaned/greetings-wordsmiths.txt","data/cleaned/wordsmiths.txt",
         "data/cleaned/closings-wordsmiths.txt","gen.txt",par_min=2,par_max=5,sen_min=2,sen_max=6)
-  elif (choice == 'Medium Formal Naive'):
+  elif (choice == 'Wordsmiths Naive'):
     naive_tags.replace_and_output("data/naive/wordsmiths.outline",
         "gen.txt", "data/naive/wordsmiths.yaml")
-  elif (choice == 'Medium Formal Word Replacement'):
+  elif (choice == 'Wordsmiths Word Replacement'):
     markov.generate_email("data/cleaned/greetings-wordsmiths.txt","data/cleaned/wordsmiths.txt",
         "data/cleaned/closings-wordsmiths.txt","gen.txt",replace=True,par_min=2,par_max=5,sen_min=2,sen_max=6)
-  elif (choice == 'Informal Markov Chain'):
+  elif (choice == 'Outings Markov Chain'):
     markov.generate_email("data/cleaned/greetings-outing.txt","data/cleaned/outing.txt",
         "data/cleaned/closings-outing.txt","gen.txt",par_min=2,par_max=4,sen_min=1,sen_max=4)
-  elif (choice == 'Informal Naive'):
+  elif (choice == 'Outings Naive'):
     naive_tags.replace_and_output("data/naive/outing.outline",
         "gen.txt", "data/naive/outing.yaml")
   else:
