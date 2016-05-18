0e4f2c2fb6741b23c3e96846096813b7213560a3
Angela Assante <angelaassante@MPB-NI-Office.local>
2016-05-18 16:25:49 -0400

    updated jumble

--- scripts/jumble.py
+++ scripts/jumble.py
@@ -1,17 +1,43 @@
 import random
+import yaml
+from collections import defaultdict
+from yaml.representer import Representer
 from nltk.corpus import wordnet as wn
-import markov
 
-replacement_prob = 30 # percent
+with open("data.yaml", 'r') as f:
+    try:
+        tag_data = yaml.load(f.read())
+    except yaml.YAMLError as exc:
+        print(exc)
 
-#def identify_noun(word):
+noun_list = tag_data["NN"]
+adjective_list = tag_data["JJ"]
+adverb_list = tag_data["RB"]
 
-def replace_noun(tagged_word):
-    word = wn.synsets(tagged_word[:-3])[0]
+noun_replacement_prob = 30 # percent
+adjective_replacement_prob = 40 # percent
+adverb_replacement_prob = 20 # percent
+
+def is_replaceable_noun(word):
+    if(len(wn.synsets(word)) > 0 and
+       len(wn.synsets(word)[0].hypernyms()) > 0 and
+       word in noun_list):
+        return True
+    else:
+        return False
+
+def switch_noun(word):
+    word = wn.synsets(word)[0]
     parent = word.hypernyms()[0]
     substitute = random.choice(parent.hyponyms())
     substitute_word = substitute.name()
-    print(wn.synset(substitute_word).name().split('.')[0])
-
-if(random.randint(1,100) <= replacement_prob):
-    replace_noun("chicken_NN")
+    new_noun = wn.synset(substitute_word).name().split('.')[0].replace('_', ' ')
+    return new_noun
+    
+def switch_by_pos(word, pos_list):
+    new_word = random.choice(pos_list)
+    # non-nouns should be standard English
+    if(len(wn.synsets(new_word)) > 0):
+        return new_word
+    else:
+        return word
